a6ebe30aaa70aff541bf0fa18bea5fb36e612186
robera16 <roberaworknehrobwork@gmail.com>
2022-03-22 14:59:25 +0300

    route setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-     Welcome to freelance
-    </div>
+    <BrowserRouter>  
+          <Header/>
+          <Switch>
+            <Route exact path="/" >
+              <Home />
+            </Route>
+            <Route path="/login" >
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+         </Switch>
+         <Footer/>
+    </BrowserRouter> 
   );
 }
 
