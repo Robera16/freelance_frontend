0d7aeebbf71d71ca927d95bd3dfa64fc1b693610
robera16 <roberaworknehrobwork@gmail.com>
2022-03-23 23:08:46 +0300

    added theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
@@ -13,17 +14,19 @@ function App() {
   return (
     <BrowserRouter>  
           <Header/>
-          <Switch>
-            <Route exact path="/" >
-              <Home />
-            </Route>
-            <Route path="/login" >
-              <Login />
-            </Route>
-            <Route path="/signup">
-              <Signup />
-            </Route>
-         </Switch>
+            <Container>
+            <Switch>
+              <Route exact path="/" >
+                <Home />
+              </Route>
+              <Route path="/login" >
+                <Login />
+              </Route>
+              <Route path="/signup">
+                <Signup />
+              </Route>
+          </Switch>
+          </Container>
          <Footer/>
     </BrowserRouter> 
   );
